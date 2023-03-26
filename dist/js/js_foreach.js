57ee49615f6244de1c221ff94a9776fa529040c1
riko0516 <r91448@gmail.com>
2023-03-26 12:36:46 +0800

    CHANGE

--- dist/js/js_foreach.js
+++ dist/js/js_foreach.js
@@ -20,9 +20,39 @@ console.log(student['like-food']);
 
 
 (() => {
+    // console.log('情境一')
+    // let num1 = 10;
+    // const run = function () {
+    //     let num2 = 20;
+    //     num1 = 30;
+    //     console.log(num1, num2);
+    // }
+
+    // run();
+    // console.log(num1);
+    // console.log(num2);
+})();
+
+(() => {
+    // console.log('情境二')
+    // let num1 = 10;
+    // const run = function (num1) {
+    //     // let num1 = xxx
+    //     let num2 = 20;
+    //     num1 = 30;
+    //     console.log(num1, num2);
+    // }
+
+    // run();
+    // console.log(num1);
+    // console.log(num2);
+})();
+
+(() => {
+    console.log('情境三')
     let num1 = 10;
     const run = function () {
-        let num2 = 20;
+        num2 = 20;
         num1 = 30;
         console.log(num1, num2);
     }
@@ -31,3 +61,20 @@ console.log(student['like-food']);
     console.log(num1);
     console.log(num2);
 })();
+
+
+(() => {
+    let student = {
+        name: 'David'
+    }
+
+    let student2 = student;
+
+    student.name = 'John';
+
+    console.log(`student name is ${student.name}, student2 name is ${student2.name}`)
+
+    student2.name = 'Dan';
+
+    console.log(`student name is ${student.name}, student2 name is ${student2.name}`)
+})()
